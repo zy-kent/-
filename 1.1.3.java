7c4935db553809c97803305fb4a74a6d4565d564
kent <364312142@qq.com>
2019-05-22 08:40:13 +0800

    Add files via upload

---
+++ 1.1.3.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(maina(2,1,1));
+    }
+    public static String maina(int a, int b, int c) {
+        return a == b && b == c ? "hello" : "world";
+    }
+}
